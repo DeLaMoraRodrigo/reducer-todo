dfebdaa81ee1c00958a4c96e219285ae8b5e8596
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-04-13 15:43:33 -0700

    Created todolist using reducer.

---
+++ todo/src/components/TodoForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const TodoForm = ({ dispatch }) => {
+  const [item, setItem] = useState("");
+
+  const handleChanges = e => {
+    setItem(e.target.value);
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_TODO",
+      payload: item
+    });
+    setItem("");
+  };
+
+  const clearCompleted = e => {
+    e.preventDefault();
+    dispatch({
+      type: "CLEAR_COMPLETED"
+    });
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={submitForm}>
+        <h2>TODO LIST</h2>
+        <div className="topOfForm">
+          <div className="addTodo">
+            <input
+              name="todo"
+              onChange={handleChanges}
+              value={item}
+              placeholder="Add New Todo..."
+            />
+            <button className="btn">Add</button>
+          </div>
+          <button onClick={clearCompleted}>Clear Completed</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
